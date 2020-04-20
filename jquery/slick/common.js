21fbbc2d400474c3ff5a34346599fea65d7b2d2a
続石　恵 <chikuwasystem@gmail.com>
2020-04-20 19:27:26 +0900

    add_スライドショー(プラグイン)

---
+++ jquery/slick/common.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#slider').slick({
+    // ここにオプションを追記する
+     centerPadding: '30%',
+     focusOnSelect: true,
+     speed: 1000,
+     autoplay: true,
+     autoplaySpeed: 1000,
+     dots: true,
+  });
+});
